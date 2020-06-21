d0397e3b8f34a77aec8056772535d133e6a388bf
Angelo Ortega <angeloale1@gmail.com>
2020-06-20 20:12:52 -0600

    [New] Library initialization and example maps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IgxTreemapModule, IgxDataChartInteractivityModule } from "igniteui-angular-charts";
+import { IgxTreemapComponent } from 'igniteui-angular-charts';
+import { TreeMapComponent } from './charts/tree-map/tree-map.component';
+import { MapComponent } from './charts/map/map.component';
+import { IgxGeographicMapModule } from 'igniteui-angular-maps';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TreeMapComponent,
+    MapComponent    
   ],
   imports: [
+    IgxTreemapModule,
+    IgxGeographicMapModule,
+    IgxDataChartInteractivityModule,
+    BrowserAnimationsModule,
     BrowserModule,
     AppRoutingModule
   ],
